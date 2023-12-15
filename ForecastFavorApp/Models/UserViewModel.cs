c8a1d84613cc2f88ebd1f1e29ffab2a8d1de010b
RubyIsCoding <ruby.chaulagain@yahoo.com>
2023-12-15 00:04:58 -0500

    Updated user controller action method for CRUD operation. Added a  UserViewModel to be used throughout the application. Updated user views to display correct user data.

---
+++ ForecastFavorApp/Models/UserViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ForecastFavorApp.Models
+{
+    public class UserViewModel
+    {
+        public int UserId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Username { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+
+    public class AddEditUserViewModel
+    {
+        public int? UserId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Username { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
